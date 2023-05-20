a2a19e9cc061486515d2466e3c14747a399d7b8c
DiamondOreMaster <102474184+DiamondOreMaster@users.noreply.github.com>
2023-05-20 12:13:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 const http = require('http');
 
+const data = {
+  name: 'John',
+  age: 25,
+};
+
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/api/endpoint') {
-    let data = '';
-
-    req.on('data', chunk => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      const parsedData = JSON.parse(data);
-      console.log(parsedData);
-      res.statusCode = 200;
-      res.end('Data received successfully');
-    });
-  } else {
-    res.statusCode = 404;
-    res.end('Not found');
-  }
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(data));
 });
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
